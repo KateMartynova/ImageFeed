1b93773289c0ec82622acf927f98e9d2407f2145
KateMartynova <mart-eka@yandex.ru>
2024-02-06 19:24:57 +0300

    Sprint 09 final work

--- ImageFeed/Helpers/UIColor+Extensions.swift
+++ ImageFeed/Helpers/UIColor+Extensions.swift
@@ -7,5 +7,6 @@ extension UIColor {
     static var ypBlue: UIColor { UIColor(named: "YP Blue") ?? UIColor.blue}
     static var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
     static var ypWhiteTransparent: UIColor { UIColor(named: "YP White (alpha 50)") ?? UIColor.gray }
+    static var ypGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
     static var ypWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white}
 }
