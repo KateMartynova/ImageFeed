7313265767adbef0d38df169a1001664ddc9d202
KateMartynova <mart-eka@yandex.ru>
2024-02-10 21:52:19 +0300

    Added Unsplash Auth screen

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Ekaterina Martynova on 10.02.2024.
+//
+
+import Foundation
+import UIKit
+
+
+final class AuthViewController: UIViewController {
+    
+    
+}
