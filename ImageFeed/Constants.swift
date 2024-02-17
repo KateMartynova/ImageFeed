a97ca5f1d077314398de0770ef013046f931d7ee
KateMartynova <mart-eka@yandex.ru>
2024-02-17 21:00:38 +0300

    Added Unsplash auth screen

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -7,12 +7,13 @@
 
 import Foundation
 
-private let AccessKey = "3AozZf3C-QVQCN4hDYU-SSf3_2CCa0QUu0zEMhNHZK4"
-private let SecretKey = "XeiNUE1HTIBBLybalFL511jXuFk38wBU4GnTNtKq19w"
-private let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
-private let AccessScope = "public+read_user+write_likes"
+let AccessKey = "3AozZf3C-QVQCN4hDYU-SSf3_2CCa0QUu0zEMhNHZK4"
+let SecretKey = "XeiNUE1HTIBBLybalFL511jXuFk38wBU4GnTNtKq19w"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let UnsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
 
-private var DefaultBaseURL: URL {
+var DefaultBaseURL: URL {
     guard let url = URL(string: "https://api.unsplash.com") else { preconditionFailure("wrong URL") }
     return url
 }
